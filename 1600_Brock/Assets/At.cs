3dc0ad139c1ec562121de7ea84caf81a28d344d0
unknown <10639426@CS514-24.ad.uvu.edu>
2016-01-20 09:18:22 -0700

    Variables

---
+++ 1600_Brock/Assets/At.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class At : MonoBehaviour 
+{
+	public int myInt = 0;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = AddOne (myInt);
+		Debug.Log (myInt);
+	}
+		int AddOne (int Number)
+	{
+		int ret;
+		ret = Number + 2;
+		return ret;
+	}
+}
